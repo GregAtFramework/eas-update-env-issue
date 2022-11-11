f7cd827ecf0b9aa1f81f73d13f91ae3b10ebd0c4
Greg Auger <greg@useframework.com>
2022-11-11 08:41:30 +0000

    Finish EAS Update setup

--- app.config.js
+++ app.config.js
@@ -4,6 +4,9 @@ const AppConfig = ({ config }) => {
   return {
     ...config,
     extra: {
+      eas: {
+        projectId: "d029f5cb-9b8b-4aac-bf44-99dfa3c7cceb",
+      },
       foo: process.env.BAR,
     },
   };
